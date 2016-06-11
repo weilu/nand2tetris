c469d0718d8d298e3e8359ae115bbe7c0b05d0b4
Wei Lu <wlu@quantcast.com>
2016-06-11 12:44:48 +0800

    project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,66 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(LOOP)
+
+  @SCREEN // 256 x 512
+  D=A
+  @addr
+  M=D  // addr = 16384
+
+  @i
+  M=0  // i = 0
+
+  @KBD
+  D=M
+
+  @LOOP_BLACK
+  D;JGT
+
+  @LOOP_WHITE
+  D;JEQ
+
+(LOOP_WHITE)
+  @i
+  D=M
+  @8192
+  D=D-A
+  @LOOP
+  D;JEQ // jump to beginning of the loop if screen is filled i = 8192
+
+  @addr
+  A=M
+  M=0 //RAM[addr]=0000000000000000
+
+  @i
+  M=M+1 // i++
+  @1
+  D=A
+  @addr
+  M=M+D // addr = addr + 1
+
+  @LOOP_WHITE
+  0;JMP
+
+(LOOP_BLACK)
+  @i
+  D=M
+  @8192
+  D=D-A
+  @LOOP
+  D;JEQ // jump to beginning of the loop if screen is filled i = 8192
+
+  @addr
+  A=M
+  M=-1 //RAM[addr]=1111111111111111
+
+  @i
+  M=M+1 // i++
+  @1
+  D=A
+  @addr
+  M=M+D // addr = addr + 1
+
+  @LOOP_BLACK
+  0;JMP
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,34 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@i
+M=0 // i=0
+
+@2
+M=0
+
+(LOOP)
+  @1
+  D=M
+
+  @i
+  D=M-D
+
+  @END
+  D;JEQ // terminate if i = R1
+
+  @0
+  D=M
+
+  @2
+  M=M+D // R2 = R2 + R0
+
+  @i
+  M=M+1 // i++
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
